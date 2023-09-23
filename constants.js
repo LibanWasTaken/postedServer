bf735b83e58c0017d92ced0ffe99237961f7b290
Liban MR <libanmesbah@gmail.com>
2023-09-23 15:39:56 +0600

    accessing firestore, email template

--- constants.js
+++
@@ -1,5 +0,0 @@
-export const SERVICE_ACCOUNT_JSON =
-  "./posted-1413e-firebase-adminsdk-2g2ut-308e868f58.json";
-export const DATABASE_URL = "https://posted-1413e-default-rtdb.firebaseio.com";
-export const EMAIL_ADDRESS = "somepeopledoingstuffs@gmail.com";
-export const EMAIL_PASS = "123Somepeopledoingstuffs";
--- server.js
+++ server.js
@@ -2,15 +2,44 @@ const express = require("express");
 const schedule = require("node-schedule");
 const nodemailer = require("nodemailer");
 const admin = require("firebase-admin");
+const serviceAccount = require("./posted-1413e-firebase-adminsdk-2g2ut-308e868f58.json");
 const { EMAIL_ADDRESS, EMAIL_PASS } = require("./env");
-
-// import * as constants from "./constants";
-const SERVICE_ACCOUNT_JSON =
-  "./posted-1413e-firebase-adminsdk-2g2ut-308e868f58.json";
-const DATABASE_URL = "https://posted-1413e-default-rtdb.firebaseio.com";
 
 const app = express();
 const port = process.env.PORT || 3001;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = admin.firestore();
+
+// Post info
+// const docRef = db.collection("posts").doc("DZL3b9ij1vWSZ4d72aa2");
+// docRef
+//   .get()
+//   .then((doc) => {
+//     if (doc.exists) {
+//       console.log("Document data:", doc.data());
+//     } else {
+//       console.log("No such document!");
+//     }
+//   })
+//   .catch((error) => {
+//     console.log("Error getting document:", error);
+//   });
+
+const collectionRef = db.collection("posts");
+
+collectionRef
+  .get()
+  .then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      console.log("👉", doc.id, "=>", doc.data());
+      console.log("");
+    });
+  })
+  .catch((error) => {
+    console.log("Error getting documents:", error);
+  });
 
 const currentDateInUTC = new Date().toUTCString();
 console.log(currentDateInUTC);
@@ -24,6 +53,15 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: EMAIL_ADDRESS,
     pass: EMAIL_PASS,
+  },
+});
+
+const transporterTest = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "audra.purdy@ethereal.email",
+    pass: "q1JaYNUA9YxDK3GZBg",
   },
 });
 
@@ -44,28 +82,76 @@ function sendEmail() {
     }
   });
 }
+function sendEmailTest() {
+  const mailOptions = {
+    from: EMAIL_ADDRESS,
+    to: "audra.purdy@ethereal.email",
+    subject: "Daily Email",
+    html: `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Missed Scheduled Postponement</title>
+        <style>
+            /* Add your CSS styles here */
+            div {
+                margin: 1rem;
+                font-style: italic;
+                margin-right: 1rem;
+                padding: 1rem;
+                border-left: 5px solid #ccc;
+            }
+            button {
+                background: black;
+                padding: 1rem 1.5rem;
+                color: white;
+                border: none;
+                outline: none;
+                letter-spacing: 2px;
+                text-transform: uppercase;
+                margin-top: 2rem;
+            }
+        </style>
+    </head>
+    <body>
+        <p>Hello,</p>
+        <br />
+        <p>This automated email has been configured by <strong>Liban</strong> to be dispatched in the event of a missed scheduled postponement.</p>
+        <br />
+        <p>Referring to the message below:</p>
+        <div>Yeah Hello it is I</div>
+        <br />
+        <p>Find the rest here:</p>
+        <button>Posted</button>
+    </body>
+    </html>
+`,
+  };
 
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log("🔴", error);
-  } else {
-    console.log("🟢", "Server is ready to take our messages");
-  }
-});
+  transporterTest.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Error sending email:", error);
+    } else {
+      console.log("Email sent:", info.response);
+    }
+  });
+}
 
-const job = schedule.scheduleJob("1 0 * * *", () => {
-  sendEmail();
-});
-
-// const serviceAccount = require(SERVICE_ACCOUNT_JSON);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: DATABASE_URL,
+// transporter.verify(function (error, success) {
+//   if (error) {
+//     console.log("🔴", error);
+//   } else {
+//     console.log("🟢", "Server is ready for mails");
+//     sendEmailTest();
+//   }
 // });
 
-// const db = admin.database();
-// const ref = db.ref("users/unposted");
+const job = schedule.scheduleJob("1 0 * * *", () => {
+  // sendEmail();
+  console.log("mail func running");
+});
 
 function updatePostDB(postID, userID) {
   /*
@@ -126,52 +212,6 @@ function addUserToPosted(user) {
     });
 }
 
-function deleteUser(uid) {
-  const refToDelete = db.ref("users/unposted/" + uid);
-  refToDelete
-    .remove()
-    .then(() => {
-      console.log("Data deleted successfully.");
-    })
-    .catch((error) => {
-      console.error("Error deleting data:", error);
-    });
-}
-
-// function checkDesiredDate() {
-//     const currentDate = new Date();
-//     const desiredDate = new Date('2023-05-15T00:00:00Z');
-
-//     if (currentDate >= desiredDate) {
-//       console.log('Desired date reached. Executing your function...');
-//       // yourCustomFunction();
-//     } else {
-//       console.log('Waiting for the desired date...');
-//     }
-//   }
-
-// const dailySchedule = schedule.scheduleJob('0 0 * * *', checkDesiredDate);
-
-// ref.once("value", (snapshot) => {
-//   const userData = snapshot.val();
-//   if (userData) {
-//     Object.keys(userData).forEach((userId) => {
-//       const user = userData[userId];
-//       if (user && user.info && user.info.firstName) {
-//         console.log(`User ID: ${userId}, firstName: ${user.info.firstName}`);
-//         // addUserToPosted(userData); // ✅
-
-//         // -> updatePostDB(postID, userID?)
-//       }
-//     });
-//     // deleteUser("testID"); // ✅
-//   } else {
-//     console.log("No user data found.");
-//   }
-
-//   admin.app().delete();
-// });
-
 app.get("/", (req, res) => {
   res.send("Server is running on port 3001");
 });
@@ -187,10 +227,7 @@ https://geshan.com.np/blog/2021/01/free-nodejs-hosting/
 https://console.cloud.google.com/appengine/cronjobs?project=posted-1413e
 https://glitch.com/edit/#!/first-spiced-gas?path=server.js%3A1%3A0
 https://www.digitalocean.com/community/tutorials/nodejs-cron-jobs-by-examples
-https://cron-job.org/en/
 https://reflectoring.io/schedule-cron-job-in-node/
-
-node-cron, node-schedule
 
 https://www.youtube.com/watch?v=27GoRa4d15c
 
